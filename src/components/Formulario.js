15f010aa5f263fc0ee279bf7e6670d0e251c7e25
Marcelm <marcelclavero94@gmail.com>
2020-08-28 11:25:14 +0200

    handleSubmit, validacion en Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -7,6 +7,7 @@ const Formulario = () => {
         ciudad: '',
         pais: ''
     });
+    const [error, guardarError] = useState(false);
 
     // extraer ciudad y pais
     const {ciudad, pais} = busqueda;
@@ -19,8 +20,26 @@ const Formulario = () => {
         });
     }
 
+    // cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // validacion - campos no vacíos
+        if (!ciudad.trim() || !pais.trim()) {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // pasarlo al componente principal
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p> : null}
             <div className="input-field col s12">
                 <input 
                     type="text"
@@ -49,6 +68,14 @@ const Formulario = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <input 
+                    type="submit"
+                    value="Buscar clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
         </form>
     );
